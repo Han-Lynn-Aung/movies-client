4cc0154f1b73bf5428fe027aa6c713bc12383a4e
Han-Lynn-Aung <hanlynnaung1997@gmail.com>
2023-11-13 00:44:28 +0630

    all done but need trace

--- src/App.js
+++ src/App.js
@@ -1,31 +1,66 @@
 import "./App.css";
 import api from "./api/axiosConfig";
 import { useState, useEffect } from "react";
-import Layout from "./componets/Layout";
 import { Routes, Route } from "react-router-dom";
+import Layout from "./componets/Layout";
 import Home from "./componets/home/Home";
+import Header from "./componets/header/header";
+import Trailer from "./componets/trailer/Trailer";
+import Reviews from "./componets/reviews/reviews";
+import NotFound from "./componets/notFound/NotFound";
 
 function App() {
   const [movies, setMovies] = useState();
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState([]);
 
   const getMovies = async () => {
     try {
       const response = await api.get("http://localhost:8080/api/v1/movies");
-      console.log(response.data);
+
       setMovies(response.data);
     } catch (err) {
       console.log(err);
     }
   };
+
+  const getMovieData = async (movieId) => {
+    try {
+      const response = await api.get(`/api/v1/movies/${movieId}`);
+
+      const singleMovie = response.data;
+
+      setMovie(singleMovie);
+
+      setReviews(singleMovie.reviews);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   useEffect(() => {
-    getMovies(), [];
-  });
+    getMovies();
+  }, []);
 
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/" element={<Home movies={movies} />}></Route>
+          <Route path="/Trailer/:ytTrailerId" element={<Trailer />}></Route>
+          <Route
+            path="/Reviews/:movieId"
+            element={
+              <Reviews
+                getMovieData={getMovieData}
+                movie={movie}
+                reviews={reviews}
+                setReviews={setReviews}
+              />
+            }
+          ></Route>
+          <Route path="*" element={<NotFound />}></Route>
         </Route>
       </Routes>
     </div>
